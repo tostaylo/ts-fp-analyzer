2419a7924be6412cd875ae199076b480117cc285
Torre <tostaylo@gmail.com>
2021-12-26 11:38:31 -0600

    refactor client to display data

--- public/index.js
+++ public/index.js
@@ -1,6 +1,63 @@
-const { mermaidAPI } = mermaid;
-
 (async function () {
+	const { mermaidAPI } = mermaid;
+
+	const insertGraphs = initialize(mermaidAPI);
+
+	const data = await (await fetch('./graph-data.json')).json();
+
+	mermaidAPI.render('mermaid', createGraphDefinition(data), insertGraphs);
+
+	renderMoreInfo(data);
+})();
+
+function createGraphDefinition(data) {
+	let graphDefinition = 'graph TB \n';
+
+	Object.entries(data).forEach(([key, value]) => {
+		Object.entries(value.fnCalls).forEach(([fn, fnData]) => {
+			graphDefinition += `${key}(<div id='${key}'>${key}</div>) --> ${fn}(<div id='${fn}'>${fn}</div>);\n`;
+		});
+	});
+	return graphDefinition;
+}
+
+function renderMoreInfo(data) {
+	const details = document.getElementById('details');
+
+	Object.entries(data).forEach(([key, value]) => {
+		// try event delegation here?
+		document.getElementById(key)?.addEventListener('click', () => {
+			const html = Object.entries(value).map(([key, value]) => {
+				if (!value) return;
+
+				if (Array.isArray(value)) {
+					if (value.length === 0) return;
+
+					return `<div> ${key}: ${value.map((item) => `<span>${item}</span>, `).join('')}</div>`;
+				}
+
+				if (typeof value === 'object') {
+					if (Object.keys(value).length === 0) return;
+
+					return `<div> ${key}: ${Object.entries(value)
+						.map(
+							([k, v]) =>
+								`<div>----${k}:${Object.entries(v)
+									.map(([l, x]) => `<div>--------${l}: ${x}</div>`)
+									.join('')}</div>`
+						)
+						.join('')}</div>`;
+				}
+
+				return `<div>${key}: ${value}</div>`;
+			});
+
+			details.innerHTML = html.join('');
+		});
+	});
+}
+
+function initialize(mermaidAPI) {
 	mermaidAPI.initialize({
 		startOnLoad: true,
 	});
@@ -11,26 +68,5 @@ const { mermaidAPI } = mermaid;
 		bindFunctions(root);
 	};
 
-	const data = await (await fetch('./graph-data.json')).json();
-
-	let graphDefinition = 'graph TB \n';
-
-	Object.entries(data).forEach(([key, value]) => {
-		Object.entries(value.fnCalls).forEach(([fn, fnData]) => {
-			graphDefinition += `${key}(<div id='${key}'>${key}</div>) --> ${fn}(<div id='${fn}'>${fn}</div>);\n`;
-		});
-	});
-
-	mermaidAPI.render('mermaid', graphDefinition, insertGraphs);
-
-	const details = document.getElementById('details');
-
-	Object.entries(data).forEach(([key, value]) => {
-		// try event delegation here?
-		document.getElementById(key)?.addEventListener('click', () => {
-			const html = Object.entries(value).map(([key, value]) => `<div>${key}: ${value}</div>`);
-
-			details.innerHTML = html.join('');
-		});
-	});
-})();
+	return insertGraphs;
+}
